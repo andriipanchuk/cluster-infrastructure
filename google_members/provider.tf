2dbaac618a82426f3cf55744a363577709df3a3f
Chyngyzkan Samatov <csamatov96@bastion-fuchicorp.us-central1-a.c.sunlit-monolith-271503.internal>
2020-04-02 17:48:14 +0000

    added

--- google_members/provider.tf
+++ google_members/provider.tf
@@ -2,4 +2,5 @@ provider "google" {
   credentials = "${file("fuchicorp_service_account.json")}"
   project     = "${var.google_project_id}"
   region      = "us-central1"
+  version     = "2.12.0"
 }
