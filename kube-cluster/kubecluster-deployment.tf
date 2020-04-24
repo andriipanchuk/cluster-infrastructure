d3967e84395f4604bf91d492d9ae3afd727f4cf7
tunji57 <tunji57@gmail.com>
2020-04-24 11:11:00 -0500

    changed region

--- kube-cluster/kubecluster-deployment.tf
+++ kube-cluster/kubecluster-deployment.tf
@@ -1,16 +1,18 @@
+data "google_container_engine_versions" "cluster_version" {
+  region       = "us-central1-a"
+}
+
 provider "google" {
   credentials = "${file("./fuchicorp-service-account.json")}" #GOOGLE_CREDENTIALS to the path of a file containing the credential JSON
   project     = "${var.google_project_id}"
 }
-data "google_container_engine_versions" "cluster_version" {
-  location       = "us-central1-a"
-}
 
-resource "google_container_cluster" "fuchicorp-cluster" {
+
+resource "google_container_cluster" "tunscotech-cluster" {
   name               = "${var.cluster_name}"
   network            = "default"
   subnetwork         = "default"
-  location           = "${var.region}"
+  region             = "${var.region}"
   min_master_version = "${data.google_container_engine_versions.cluster_version.latest_node_version}"
   initial_node_count = "${var.node_count}"
   project            = "${var.google_project_id}"
