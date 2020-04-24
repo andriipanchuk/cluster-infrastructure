3f9577a56bee07b6cccb557406ddf424b0f962e1
tunji57 <tunji57@gmail.com>
2020-04-24 11:21:38 -0500

    reverted

--- kube-cluster/google_bucket.tf
+++ kube-cluster/google_bucket.tf
@@ -1,5 +1,5 @@
 resource "google_storage_bucket" "olabucketss" {
   name          = "${var.google_bucket_name}"
   storage_class = "COLDLINE"
-  region      = "${var.region}"
+  location      = "${var.region}"
 }
